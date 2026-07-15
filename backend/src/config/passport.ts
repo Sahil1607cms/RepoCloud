299fc6e5afa32b7eb0a88cf153f26628fd393a92
Sahil <sahil16072004@gmail.com>
2026-07-16 01:55:05 +0530

    update passport.ts

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -8,7 +8,7 @@ passport.use(
       clientID: process.env.GITHUB_CLIENT_ID || "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
       callbackURL:
-        "http://localhost:3000/auth/github/callback",
+        process.env.GITHUB_CALLBACK_URL || "http://localhost:3000/auth/github/callback",
     },
     async (accessToken: string, refreshToken: string, profile: any, done: any) => {
       return done(null, profile);
