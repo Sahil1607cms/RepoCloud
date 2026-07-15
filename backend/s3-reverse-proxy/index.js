9748679d963e2d1ec4061a834084b9ca69560d26
Sahil <sahil16072004@gmail.com>
2026-07-16 00:30:22 +0530

    s3 server code fix

--- backend/s3-reverse-proxy/index.js
+++ backend/s3-reverse-proxy/index.js
@@ -1,38 +1,50 @@
-import express from 'express'
-import httpProxy from 'http-proxy'
-import dotenv from 'dotenv'
+import express from "express";
+import httpProxy from "http-proxy";
+import dotenv from "dotenv";
 
-dotenv.config({ path: '../.env' })
-const app = express()
+dotenv.config({ path: "../.env" });
+const app = express();
 
-const PORT =process.env.S3_PORT || 8000
+const PORT = process.env.PORT || process.env.S3_PORT || 8000;
 
-const proxy = httpProxy.createProxy() //creating the proxy, it can forward request to another server
+const proxy = httpProxy.createProxy(); //creating the proxy, it can forward request to another server
 
-console.log(`Base path : ${process.env.BASE_PATH}`)
 if (!process.env.BASE_PATH) {
   throw new Error("BASE_PATH is not defined");
 }
 
 //middleware
 //handle all requests without a path also
-app.use((req,res)=>{
-    const hostname  = req.hostname              // abc123.localhost:8000 => abc123.localhost
-    const projectId = hostname.split('.')[0];   // abc123
- 
-    const basePath = process.env.BASE_PATH.replace(/\/$/, "");
-    const resolvesTo = `${basePath}/${projectId}`
-    return proxy.web(req,res, {target:resolvesTo,changeOrigin:true}) 
-    //changeOrigin changes the Host header of the proxied request to match the target server, making the request appear as if it was sent directly to that server.
+app.use((req, res) => {
+  // Example:
+  // /abc123
+  // /abc123/
+  // /abc123/assets/index.js
 
+  const parts = req.path.split("/").filter(Boolean);
 
-})
+  if (parts.length === 0) {
+    return res.status(400).send("Missing project id");
+  }
+  const projectId = parts[0];
+  const basePath = process.env.BASE_PATH.replace(/\/$/, "");
+  const target = `${basePath}/${projectId}`;
 
-proxy.on('proxyReq', (proxyReq, req, res) => { //proxyReq build in event, runs just before proxy sends the request to S3
-    const url = req.url;
-    if (url === '/')
-        proxyReq.path += 'index.html' //changing /__outputs__/abc123/ => /__outputs__/abc123/index.html just before sending
+  // Remove "/abc123" before forwarding to S3
+  req.url = req.url.replace(`/${projectId}`, "") || "/";
 
-})
+  proxy.web(req, res, {
+    target,
+    changeOrigin: true,
+  });
+});
 
-app.listen(PORT, ()=> console.log(`Reverse proxy server running on port ${PORT}`))
+proxy.on("proxyReq", (proxyReq, req, res) => {
+  //proxyReq build in event, runs just before proxy sends the request to S3
+  const url = req.url;
+  if (url === "/") proxyReq.path += "/index.html"; //changing /__outputs__/abc123/ => /__outputs__/abc123/index.html just before sending
+});
+
+app.listen(PORT, () =>
+  console.log(`Reverse proxy server running on port ${PORT}`),
+);
