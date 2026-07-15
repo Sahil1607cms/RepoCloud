df203fe0d00c608b8d52ec3dc6523dc259ac811a
Sahil <sahil16072004@gmail.com>
2026-07-16 01:11:41 +0530

    frontend deployement fix

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,11 +1,11 @@
-import api from "./api.js";
+import authApi from "./authApi.js";
 
 //creating objects with authentication objects
 export const authService = {
   //for hydrating the global variables using context api (AuthContext)
   getCurrentUser: async () => {
     try {
-      const response = await api.get("/auth/me");
+      const response = await authApi.get("/auth/me");
       // console.log(response.data) 
       // = {id: '149762467', username: 'Sahil1607cms', avatar_url: 'https://avatars.githubusercontent.com/u/149762467?v=4', displayName: 'Sahil Srivastava'}
 
@@ -22,7 +22,7 @@ export const authService = {
   // Logout 
   logout: async () => {
     try {
-      await api.post("/auth/logout");
+      await authApi.post("/auth/logout");
     } catch (error) {
       console.error("Failed to logout:", error);
       throw error;
@@ -31,13 +31,13 @@ export const authService = {
 
   // not api request, redirecting to github login page
   loginWithGithub: () => {
-    window.location.href = `${import.meta.env.VITE_API_URL || "http://localhost:3000"}/auth/github`;
+   window.location.href = `${import.meta.env.VITE_AUTH_URL}/auth/github`;
   },
 
   // Check if user is authenticated
   isAuthenticated: async () => {
     try {
-      await api.get("/auth/me");
+      await authApi.get("/auth/me");
       return true;
     } catch (error) {
       return false;
